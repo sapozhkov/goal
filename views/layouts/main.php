b05c871941e7265284adf9785944e8e12afbae1a
Alexandr Sapozhkov <sapozhkov@twinscom.ru>
2015-10-26 14:17:54 +0300

    app version added

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -63,7 +63,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; GoalList v<?= Yii::$app->version ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
