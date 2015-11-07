603bc210e404c11f59c59329b0bbd0e8b9b2ee3e
sapozhkov <a@sapozhkov.ru>
2015-11-08 01:46:39 +0300

    #3 created migration for log table

---
+++ migrations/m151107_223818_log.php
@@ -0,0 +1,26 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m151107_223818_log extends Migration
+{
+    public function up()
+    {
+        $this->createTable('log', [
+            'id' => Schema::TYPE_PK,
+            'goal_id' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
+            'created_at' => Schema::TYPE_DATETIME . ' NOT NULL',
+            'data' => Schema::TYPE_TEXT . ' NOT NULL',
+            'message' => Schema::TYPE_TEXT . ' NOT NULL',
+        ], 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB');
+
+        $this->addForeignKey('log2goal', 'log', 'goal_id', 'goal', 'id', 'RESTRICT', 'CASCADE');
+    }
+
+    public function down()
+    {
+        $this->delete('log');
+    }
+
+}
