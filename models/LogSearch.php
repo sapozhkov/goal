d675ea14ee79aa08d8f1c32d445200e91ec53bc3
sapozhkov <a@sapozhkov.ru>
2016-05-06 20:28:56 +0300

    #16 added log controller

--- models/LogSearch.php
+++ models/LogSearch.php
@@ -5,13 +5,22 @@ namespace app\models;
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use yii\helpers\ArrayHelper;
 
 /**
  * LogSearch represents the model behind the search form about `app\models\Log`.
  */
 class LogSearch extends Log
 {
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'goal_id'], 'integer'],
+            [['created_at', 'data', 'message'], 'safe'],
+        ];
+    }
 
     /**
      * @inheritdoc
@@ -33,17 +42,26 @@ class LogSearch extends Log
     {
         $query = Log::find();
 
-        $query->orderBy('created_at DESC');
-
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
         ]);
 
         $this->load($params);
 
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
         $query->andFilterWhere([
-            'goal_id' => ArrayHelper::getValue($params, 'goal_id'),
+            'id' => $this->id,
+            'goal_id' => $this->goal_id,
+            'created_at' => $this->created_at,
         ]);
+
+        $query->andFilterWhere(['like', 'data', $this->data])
+            ->andFilterWhere(['like', 'message', $this->message]);
 
         return $dataProvider;
     }
