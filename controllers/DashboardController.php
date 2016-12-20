20e253c9f0e67e35ab4082d1de0100e397b49ac8
Alexandr Sapozhkov <sapozhkov@twinscom.ru>
2016-12-20 15:20:03 +0300

    #40 extract dashboard methods to a separate class

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -2,80 +2,31 @@
 
 namespace app\controllers;
 
-use app\models\Goal;
-use app\models\Task;
+use app\helper\Dashboard;
 
 class DashboardController extends \yii\web\Controller
 {
     public function actionIndex()
     {
         // nearest tasks
-        $taskQuery = Task::find()
-            ->where(['closed' => 0])
-            ->orderBy('date')
-            ->limit(10)
-        ;
-        $tasksCount = $taskQuery->count();
-        $tasks = $taskQuery->andWhere('date')->all();
+        $tasks = Dashboard::getTasksNearest($tasksCount);
 
         // tasks without date
-        $tasksWithoutDateQuery = Task::find()
-            ->where([
-                'closed' => 0,
-                'date' => null
-            ])
-            ->orderBy('id')
-            ->limit(10)
-        ;
-        $tasksWithoutDateCount = $tasksWithoutDateQuery->count();
-        $tasksWithoutDate = $tasksWithoutDateQuery->all();
+        $tasksWithoutDate = Dashboard::getTasksWithoutDate($tasksWithoutDateCount);
 
         // overdue tasks
-        $tasksOverdueQuery = Task::find()
-            ->where(['closed' => 0])
-            ->andWhere('date < DATE(NOW())')
-            ->orderBy('date')
-            ->limit(10)
-        ;
-        $tasksOverdueCount = $tasksOverdueQuery->count();
-        $tasksOverdue = $tasksOverdueQuery->all();
-
+        $tasksOverdue = Dashboard::getTasksOverdue($tasksOverdueCount);
 
         /********************************************************************/
 
         // nearest goals
-        $goalQuery = Goal::find()
-            ->innerJoinWith('status')
-            ->where('status.closed=0')
-            ->orderBy('to_be_done_at')
-            ->limit(7)
-        ;
-        $goalsCount = $goalQuery->count();
-        $goals = $goalQuery->andWhere('to_be_done_at')->all();
+        $goals = Dashboard::getGoalsNearest($goalsCount);
 
         // goals without date
-        $goalsWithoutDateQuery = Goal::find()
-            ->innerJoinWith('status')
-            ->where([
-                'status.closed' => 0,
-                'to_be_done_at' => null
-            ])
-            ->orderBy('goal.id')
-            ->limit(10)
-        ;
-        $goalsWithoutDateCount = $goalsWithoutDateQuery->count();
-        $goalsWithoutDate = $goalsWithoutDateQuery->all();
+        $goalsWithoutDate = Dashboard::getGoalsWithoutDate($goalsWithoutDateCount);
 
         // overdue goals
-        $goalsOverdueQuery = Goal::find()
-            ->innerJoinWith('status')
-            ->where('status.closed=0')
-            ->andWhere('to_be_done_at < DATE(NOW())')
-            ->orderBy('goal.id')
-            ->limit(10)
-        ;
-        $goalsOverdueCount = $goalsOverdueQuery->count();
-        $goalsOverdue = $goalsOverdueQuery->all();
+        $goalsOverdue = Dashboard::getGoalsOverdue($goalsOverdueCount);
 
         return $this->render('index', [
             'tasks' => $tasks,
---
+++ helper/Dashboard.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace app\helper;
+
+
+use app\models\Goal;
+use app\models\Task;
+
+/**
+ * Dashboard Helper
+ */
+class Dashboard {
+
+    /**
+     * Returns first overdue tasks
+     * @param int $iTotalCount if provided total count will be returned to this value
+     * @return Task[]
+     */
+    public static function getTasksOverdue(&$iTotalCount=0 ) {
+
+        $query = Task::find()
+            ->where(['closed' => 0])
+            ->andWhere('date < DATE(NOW())')
+            ->orderBy('date')
+            ->limit(10)
+        ;
+
+        if ( func_num_args() )
+            $iTotalCount = $query->count();
+
+        return $query->all();
+
+    }
+
+    /**
+     * Returns first tasks without date
+     * @param int $iTotalCount if provided total count will be returned to this value
+     * @return Task[]
+     */
+    public static function getTasksWithoutDate( &$iTotalCount=0 ) {
+
+        $query = Task::find()
+            ->where([
+                'closed' => 0,
+                'date' => null
+            ])
+            ->orderBy('id')
+            ->limit(10)
+        ;
+        if ( func_num_args() )
+            $iTotalCount = $query->count();
+
+        return $query->all();
+
+    }
+
+    /**
+     * Returns first nearest tasks
+     * @param int $iTotalCount if provided total count will be returned to this value
+     * @return Task[]
+     */
+    public static function getTasksNearest(&$iTotalCount=0 ) {
+
+        $query = Task::find()
+            ->where(['closed' => 0])
+            ->orderBy('date')
+            ->limit(10)
+        ;
+
+        if ( func_num_args() )
+            $iTotalCount = $query->count();
+
+        return $query->andWhere('date')->all();
+
+    }
+
+    /**
+     * Returns first nearest goals
+     * @param int $iTotalCount if provided total count will be returned to this value
+     * @return Goal[]
+     */
+    public static function getGoalsNearest(&$iTotalCount=0 ) {
+
+        $query = Goal::find()
+            ->innerJoinWith('status')
+            ->where('status.closed=0')
+            ->orderBy('to_be_done_at')
+            ->limit(7)
+        ;
+
+        if ( func_num_args() )
+            $iTotalCount = $query->count();
+
+        return $query->andWhere('to_be_done_at')->all();
+
+    }
+
+    /**
+     * Returns first goals without date
+     * @param int $iTotalCount if provided total count will be returned to this value
+     * @return Goal[]
+     */
+    public static function getGoalsWithoutDate( &$iTotalCount=0 ) {
+
+        $query = Goal::find()
+            ->innerJoinWith('status')
+            ->where([
+                'status.closed' => 0,
+                'to_be_done_at' => null
+            ])
+            ->orderBy('goal.id')
+            ->limit(10)
+        ;
+
+        if ( func_num_args() )
+            $iTotalCount = $query->count();
+
+        return $query->all();
+
+    }
+
+    /**
+     * Returns first overdue goals
+     * @param int $iTotalCount if provided total count will be returned to this value
+     * @return Goal[]
+     */
+    public static function getGoalsOverdue(&$iTotalCount=0 ) {
+
+        $query = Goal::find()
+            ->innerJoinWith('status')
+            ->where('status.closed=0')
+            ->andWhere('to_be_done_at < DATE(NOW())')
+            ->orderBy('goal.id')
+            ->limit(10)
+        ;
+
+        if ( func_num_args() )
+            $iTotalCount = $query->count();
+
+        return $query->all();
+
+    }
+
+}
