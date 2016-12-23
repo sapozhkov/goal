6a3d317914671a41e8b1d72f5ab90fa7b44a7ca1
Alexandr Sapozhkov <sapozhkov@twinscom.ru>
2016-12-23 11:37:18 +0300

    #39 counters migration refactoring

--- migrations/m161222_072411_counters.php
+++ migrations/m161222_072411_counters.php
@@ -1,6 +1,5 @@
 <?php
 
-use yii\db\Schema;
 use yii\db\Migration;
 
 class m161222_072411_counters extends Migration
@@ -9,21 +8,21 @@ class m161222_072411_counters extends Migration
     {
 
         $this->createTable('counter', [
-            'id' => Schema::TYPE_PK,
-            'goal_id' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
-            'title' => Schema::TYPE_STRING . '(256) NOT NULL',
-            'type' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
-            'description' => Schema::TYPE_TEXT. ' NOT NULL',
+            'id' => $this->primaryKey(),
+            'goal_id' => $this->integer()->notNull(),
+            'title' => $this->string()->notNull(),
+            'type' => $this->integer()->notNull(),
+            'description' => $this->text()->notNull(),
         ], 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB');
 
         $this->addForeignKey('counter2goal', 'counter', 'goal_id', 'goal', 'id', 'RESTRICT', 'CASCADE');
 
         $this->createTable('counter_row', [
-            'id' => Schema::TYPE_PK,
-            'counter_id' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
-            'time' => Schema::TYPE_DATETIME . ' NOT NULL',
-            'value' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
-            'description' => Schema::TYPE_TEXT. ' NOT NULL',
+            'id' => $this->primaryKey(),
+            'counter_id' => $this->integer()->notNull(),
+            'time' => $this->dateTime()->notNull(),
+            'value' => $this->integer()->notNull(),
+            'description' => $this->text()->notNull(),
         ], 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB');
 
         $this->addForeignKey('row2counter', 'counter_row', 'counter_id', 'counter', 'id', 'RESTRICT', 'CASCADE');
@@ -32,8 +31,10 @@ class m161222_072411_counters extends Migration
 
     public function down()
     {
-        $this->dropTable('counters');
+        $this->dropForeignKey('row2counter', 'counter_row');
         $this->dropTable('counter_row');
+        $this->dropForeignKey('counter2goal', 'counter');
+        $this->dropTable('counter');
     }
 
 }
